e1f5837ef28be2759e49a5fe6ad5c39a0d2268e0
eugenegodzishevsky <109996712+eugenegodzishevsky@users.noreply.github.com>
2024-05-07 13:30:05 +1000

    no message

---
+++ Sofa_Elda900/Sofa_Elda900/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sofa_Elda900
+//
+//  Created by Vermut xxx on 07.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sofa_Elda900/Sofa_Elda900/Sofa_Elda900App.swift
@@ -0,0 +1,17 @@
+//
+//  Sofa_Elda900App.swift
+//  Sofa_Elda900
+//
+//  Created by Vermut xxx on 07.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Sofa_Elda900App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
